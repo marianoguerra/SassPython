6c72f3dac864cf43407c65cae825afe7a36e311e
Mariano Guerra <luismarianoguerra@gmail.com>
2012-07-27 14:51:42 +0200

    initial import

---
+++ README.rst
@@ -0,0 +1,129 @@
+SassPython - bindings for libsass
+=================================
+
+why?
+----
+
+* the guy on this talk asqued for it: http://www.confreaks.com/videos/859-railsconf2012-the-future-of-sass
+* I wanted to play with ctypes
+
+who?
+----
+
+marianoguerra
+
+how?
+----
+
+first of all download, compile and install libsass::
+
+        git clone https://github.com/hcatlin/libsass.git
+        cd libsass
+        ./configure
+        make
+        sudo make install
+
+then you can play with this project in two ways
+
+command line
+............
+
+if no options provided read from stdin::
+
+        ➜  src  ./sass.py                           
+        table.hl td.ln {
+          text-align: right;
+        }
+
+        table.hl td.ln {
+          text-align: right; }
+        ➜  src  ./sass.py -f ../examples/simple.scss -d ~
+        usage: sass.py [-h] [-f FILE_PATH | -d DIR_PATH]
+        sass.py: error: argument -d/--dir: not allowed with argument -f/--file
+        ➜  src  
+
+from a file::
+
+        ➜  src  ./sass.py -f ../examples/simple.scss
+
+        .content-navigation {
+          border-color: #3bbfce;
+          color: darken(#3bbfce, 9%); }
+
+        .border {
+          padding: 8px;
+          margin: 8px;
+          border-color: #3bbfce; }
+
+from a folder:
+
+.. image:: http://chzscience.files.wordpress.com/2011/11/funny-science-news-experiments-memes-dog-science-fuzzy-logic.jpg
+
+::
+
+        # I think it doesn't work, never used sass before and don't know what
+        # this means :)
+        ➜  src  ./sass.py -d ../examples/
+
+code
+....
+
+from a string::
+
+        Python 2.7.3 (default, Apr 20 2012, 22:44:07) 
+
+        >>> import sass
+        >>> STYLE = """
+        ... table.hl td.ln {
+        ...   text-align: right;
+        ... }
+        ... """
+
+        >>> ok, style = sass.compile(STYLE)
+
+        >>> ok
+        True
+
+        >>> print style
+        table.hl td.ln {
+          text-align: right; }
+
+from a file::
+
+        >>> ok, style = sass.compile_path("../examples/simple.scss")
+
+        >>> ok
+        True
+
+        >>> print style
+        .content-navigation {
+          border-color: #3bbfce;
+          color: darken(#3bbfce, 9%); }
+
+        .border {
+          padding: 8px;
+          margin: 8px;
+          border-color: #3bbfce; }
+
+from a folder::
+
+        >>> ok, style = sass.compile_folder("../examples/")
+        
+        # ???
+        # Profit!
+
+
+license?
+--------
+
+MIT + optional beer for the creator
+
+what's left to do?
+------------------
+
+* lookup libsass more inteligently
+* python 3 support (should be easy)
+* package it and submit it to pypy
+* make the folder stuff work
+* add command line options to specify option styles
+* see what the return value of the compile_* means and use it if needed
---
+++ src/sass.py
@@ -0,0 +1,231 @@
+#!/usr/bin/env python
+from ctypes import *
+
+import sys
+import argparse
+
+LIB = cdll.LoadLibrary("/usr/local/lib/libsass.so")
+
+class Style():
+    # define SASS_STYLE_NESTED     0
+    # define SASS_STYLE_EXPANDED   1
+    # define SASS_STYLE_COMPACT    2
+    # define SASS_STYLE_COMPRESSED 3
+
+    NESTED     = 0
+    EXPANDED   = 1
+    COMPACT    = 2
+    COMPRESSED = 3
+
+class Options(Structure):
+    """
+    struct sass_options {
+      int output_style;
+      char* include_paths;
+    };
+    """
+
+    def __init__(self, output_style=Style.NESTED, include_paths=""):
+        self.output_style = output_style
+        self.include_paths = include_paths
+
+    _fields_ = [
+        ("output_style", c_int),
+        ("include_paths", c_char_p)
+    ]
+
+class Context(Structure):
+    """
+    struct sass_context {
+      char* source_string;
+      char* output_string;
+      struct sass_options options;
+      int error_status;
+      char* error_message;
+    };
+    """
+
+    _fields_ = [
+        ("source_string", c_char_p),
+        ("output_string", c_char_p),
+        ("options", Options),
+        ("error_status", c_int),
+        ("error_message", c_char_p)
+    ]
+
+    def init(self, source_string=""):
+        self.source_string = source_string
+        self.output_string = ""
+        self.options = Options()
+        self.error_status = 0
+        self.error_message = ""
+
+    def __str__(self):
+        return '<context source="{source_string} output="{output_string}" status="{error_status}" error="{error_message}>"'.format(
+                source_string=self.source_string,
+                output_string=self.output_string,
+                error_status=self.error_status,
+                error_message=self.error_message)
+
+class FileContext(Structure):
+    """
+    struct sass_file_context {
+      char* input_path;
+      char* output_string;
+      struct sass_options options;
+      int error_status;
+      char* error_message;
+    };
+    """
+
+    def init(self, input_path=""):
+        self.input_path = input_path
+        self.output_string = ""
+        self.options = Options()
+        self.error_status = 0
+        self.error_message = ""
+
+    _fields_ = [
+        ("input_path", c_char_p),
+        ("output_string", c_char_p),
+        ("options", Options),
+        ("error_status", c_int),
+        ("error_message", c_char_p)
+    ]
+
+class FolderContext(Structure):
+    """
+    struct sass_folder_context {
+      char* search_path;
+      char* output_path;
+      struct sass_options options;
+      int error_status;
+      char* error_message;
+    };
+    """
+
+    def init(self, search_path=""):
+        self.search_path = search_path
+        self.output_string = ""
+        self.options = Options()
+        self.error_status = 0
+        self.error_message = ""
+
+    _fields_ = [
+        ("search_path", c_char_p),
+        ("output_string", c_char_p),
+        ("options", Options),
+        ("error_status", c_int),
+        ("error_message", c_char_p)
+    ]
+
+_new_context = LIB.sass_new_context
+_new_context.restype = Context
+
+_new_file_context = LIB.sass_new_file_context
+_new_file_context.restype = FileContext
+
+_new_folder_context = LIB.sass_new_folder_context
+_new_folder_context.restype = FolderContext
+
+_free_context = LIB.sass_free_context
+_free_context.argtypes = [POINTER(Context)]
+
+_compile = LIB.sass_compile
+_compile.restype = c_int
+_compile.argtypes = [POINTER(Context)]
+
+_compile_file = LIB.sass_compile_file
+_compile_file.restype = c_int
+_compile_file.argtypes = [POINTER(FileContext)]
+
+_compile_folder = LIB.sass_compile_folder
+_compile_folder.restype = c_int
+_compile_folder.argtypes = [POINTER(FolderContext)]
+
+def compile(scss):
+    """
+    compile sass code contained in *scss* (a string)
+    return a tuple with the status as a boolean as first item and
+    the content as second (the error message if status is False, compiled style
+    if True)
+    """
+    ctx = _new_context()
+    ctx.init(scss)
+
+    result = _compile(ctx)
+
+    if ctx.error_status:
+        return False, ctx.error_message
+    else:
+        return True, ctx.output_string
+
+def compile_path(path):
+    """
+    compile sass code contained in a file at *path* (a string)
+    return a tuple with the status as a boolean as first item and
+    the content as second (the error message if status is False, compiled style
+    if True)
+    """
+    fctx = _new_file_context()
+    fctx.init(path)
+
+    result = _compile_file(fctx)
+
+    if fctx.error_status:
+        return False, fctx.error_message
+    else:
+        return True, fctx.output_string
+
+def compile_folder(path):
+    """
+    compile sass code contained in a folder at *path* (a string)
+    return a tuple with the status as a boolean as first item and
+    the content as second (the error message if status is False, compiled style
+    if True)
+    """
+
+    dctx = _new_folder_context()
+    dctx.init(path)
+
+    result = _compile_folder(dctx)
+
+    if dctx.error_status:
+        return False, dctx.error_message
+    else:
+        return True, dctx.output_string
+
+def build_arg_parser():
+    """return an argument parser object"""
+    parser = argparse.ArgumentParser()
+
+    group = parser.add_mutually_exclusive_group()
+
+    group.add_argument('-f', '--file', type=str, default=None, dest="file_path",
+        help="file to convert")
+    group.add_argument('-d', '--dir', type=str, default=None, dest="dir_path",
+        help="directory to convert")
+
+    return parser
+
+def main():
+    """main funcion if called from the command line"""
+    parser = build_arg_parser()
+    opts = parser.parse_args()
+
+    if opts.file_path is not None:
+        ok, out = compile_path(opts.file_path)
+    elif opts.dir_path is not None:
+        ok, out = compile_folder(opts.dir_path)
+    else:
+        ok, out = compile(sys.stdin.read())
+
+    status = 0 if ok else 1
+
+    sys.stdout.write("\n")
+    sys.stdout.write(out)
+    sys.stdout.flush()
+    sys.exit(status)
+
+if __name__ == "__main__":
+    main()
